d9e70ab6ee858374510aab85a1c198ad972994e2
SpencerELewis <spencerlewis12304@gmail.com>
2025-12-29 19:49:27 -0600

    Keyboard handles backspace

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -14,11 +14,14 @@ print:
 
 wait_key:
     call keyboard_handler ; Wait for keypress, result in AL
+    cmp al, 0x08        ; Was it Backspace?
+    je wait_key         ; If so, don't print again, just wait for next key. Otherwise, you'll print backspace twice and skip keys
     mov ah, 0x0E
     int 0x10 ; Print pressed key
     jmp wait_key ; Loop back to wait for next key
 
-msg: db 0x0D, 0x0A, 'JerichOS boot initiated', 0x0D, 0x0A, 0x0D, 0x0A, 0 ; Null-terminated string
+msg: db 0x0D, 0x0A, 'JerichOS boot initiated', 0x0D, 0x0A, 0x0D, 0x0A, 0 
+; Null-terminated string, with 0x0D carriage return and 0x0A line feed to skip lines
 
 %include "keyboard.asm"
 
